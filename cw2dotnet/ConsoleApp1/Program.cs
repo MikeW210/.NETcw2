6b10f783e111c590a21c14ede0250be86ca97720
MikeW210 <44272309+MikeW210@users.noreply.github.com>
2019-06-10 20:43:09 +0200

    Add files via upload

--- cw2dotnet/ConsoleApp1/Program.cs
+++ cw2dotnet/ConsoleApp1/Program.cs
@@ -8,64 +8,45 @@ namespace ConsoleApp1
 {
     class Program
     {
-     
-        private static int[] tab = new int[5];
+
+        private static int[] tab;
         private static int index = 0;
 
         static void Main(string[] args)
         {
+            Tablica tab = new Tablica();
             bool czyKoniec = false;
-            
 
             while (czyKoniec == false)
             {
                 Console.WriteLine("Podaj liczbe którą chcesz wpisać do tablicy: ");
                 int numerek = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine(Environment.NewLine);
-                add(numerek);
-                Console.WriteLine("Wielkość tablicy: " + tab.Length);
-                for(int i =0; i<index; i++)
+                tab.add(numerek);
+
+                tab.printAllElements();
+
+                
+                
+                Console.WriteLine("Chcesz kontynuowac? 1-tak 2-nie");
+                int czy = Convert.ToInt32(Console.ReadLine());
+                if (czy == 1)
                 {
-                    Console.WriteLine(tab[i]);
+                    czyKoniec = false;
                 }
-
-
-
+                else
+                {
+                    czyKoniec = true;      
+                }
+                
             }
+
+           
+            Environment.Exit(0);
         }
 
 
-
-        static void add(int x)
-        {
-            if(index >= tab.Length)
-            {
-                wyjatek(x);
-                makeItBigger(2);
-                tab[index++] = x;
-            }
-            else
-            {
-                tab[index++] = x;
-            } 
-        }
-
-        static void makeItBigger(int x)
-        {
-            int[] tab2 = new int[tab.Length];
-            Array.Copy(tab, tab2, index);
-            tab = new int[x * tab.Length];
-            Array.Copy(tab2, tab, index);
-
-        }
-        static int wyjatek(int x)
-        {
-            if (x >= tab.Length)
-            {
-                throw new System.IndexOutOfRangeException("Wyleciałeś poza skale!");
-            }
-            return tab[x];
-        }
+        
 
     }
 }
---
+++ cw2dotnet/ConsoleApp1/Tablica.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public delegate void MyDelegate(int tabSize);
+    public class Tablica
+    {
+        private MyDelegate myDelegate = null;
+        private int[] tab = new int[1];
+        private int index = 0;
+        public Tablica()
+        {
+            myDelegate += tabSize;
+
+        }
+        public Tablica(int liczba)
+        {
+            tab = new int[liczba];
+        }
+
+        public void tabSize(int sizeOfArray)
+        {
+            Console.WriteLine("Rozmiar tablicy: " + sizeOfArray);
+        }
+        
+
+
+        public void printAllElements()
+        {
+            for (int i = 0; i < index; i++)
+            {
+                wypiszTablice(i);
+            }
+
+            Console.WriteLine("TAB LENGTH: " + tab.Length);
+        }
+
+        public void add(int x)
+        {
+            if (index >= tab.Length)
+            {
+                
+                makeItBigger(2*tab.Length);
+            }
+            tab[index++] = x;
+
+        }
+
+        public void makeItBigger(int x)
+        {
+            int[] tab2 = new int[tab.Length * 2];
+            Array.Copy(tab, tab2, index);
+            tab = tab2;
+
+        }
+        public int wypiszTablice(int x)
+        {
+            if (x <= tab.Length)
+            {
+                Console.WriteLine("ID " + x + " is: " + tab[x]);
+                
+            }
+            else
+            {
+                throw new System.IndexOutOfRangeException("Wyleciałeś poza skale!");
+            }
+
+            return tab[x];
+        }
+    }
+}
